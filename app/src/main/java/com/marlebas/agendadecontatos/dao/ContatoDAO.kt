dcdea5589184d35bf7ede371968688cd7cfdd36f
Marlon <maarlonsantos17@gmail.com>
2026-09-06 23:03:23 -0300

    feat: Lista os contatos na tela principal

--- app/src/main/java/com/marlebas/agendadecontatos/dao/ContatoDAO.kt
+++ app/src/main/java/com/marlebas/agendadecontatos/dao/ContatoDAO.kt
@@ -2,6 +2,7 @@ package com.marlebas.agendadecontatos.dao
 
 import androidx.room3.Dao
 import androidx.room3.Insert
+import androidx.room3.Query
 import com.marlebas.agendadecontatos.model.Contato
 
 @Dao
@@ -9,4 +10,7 @@ interface ContatoDAO {
 
     @Insert
     fun salvar(contatos: Contato)
+
+    @Query("SELECT * FROM tabela_contatos ORDER BY nome ASC")
+    fun getContatos(): MutableList<Contato>
 }
--- app/src/main/java/com/marlebas/agendadecontatos/views/ListaContatos.kt
+++ app/src/main/java/com/marlebas/agendadecontatos/views/ListaContatos.kt
@@ -1,8 +1,12 @@
 package com.marlebas.agendadecontatos.views
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -14,17 +18,46 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.marlebas.agendadecontatos.AppDatabase
+import com.marlebas.agendadecontatos.dao.ContatoDAO
+import com.marlebas.agendadecontatos.itemlista.ContatoItem
+import com.marlebas.agendadecontatos.model.Contato
 import com.marlebas.agendadecontatos.ui.theme.PURPLE500
 import com.marlebas.agendadecontatos.ui.theme.WHITE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+private lateinit var contatoDAO: ContatoDAO
+
+@SuppressLint("CoroutineCreationDuringComposition") //Permite criar uma trhead durante a composição
 @OptIn(ExperimentalMaterial3Api::class) //Adicionar essa anotação para trabalhar com o Scaffold
 @Composable
 fun ListaContatos(navController: NavController){
+
+    val listaContatos by remember { mutableStateOf<MutableList<Contato>>(mutableListOf()) }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    scope.launch(Dispatchers.IO) {
+        contatoDAO = AppDatabase.getInstance(context).contatoDAO()
+        val contato = contatoDAO.getContatos()
+
+        for(contatos in contato){
+            listaContatos.add(contatos)
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -61,8 +94,14 @@ fun ListaContatos(navController: NavController){
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues) //Passa o paddingValues pra dentro do padding para o conteúdo ficar dentro do Scaffold
+                .background(Color.LightGray)
         ) {
-
+            LazyColumn{
+                itemsIndexed(listaContatos){
+                    _, contato ->
+                    ContatoItem(contato)
+                }
+            }
         }
 
     }
